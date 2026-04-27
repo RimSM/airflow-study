379edb03d6227d0797a82ca8dde6598a9896e4c1
RimSM <rsm9085@gmail.com>
2026-04-27 21:46:59 +0900

    명칭 변경

--- dags/dags_python_with_op_arg.py
+++ dags/dags_python_with_op_arg.py
@@ -7,7 +7,7 @@ from airflow.decorators import task
 
 
 with DAG(
-    dag_id="dags_python_task_decorator",
+    dag_id="dags_python_with_op_arg",
     schedule= "0 2 * * 1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False

8320440a5f63dfab566431dde517da14c753c3f7
RimSM <rsm9085@gmail.com>
2026-04-29 22:04:21 +0900

    수정

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -18,4 +18,15 @@ with DAG(
     @task (task_id = 'python_xcom_pull_1')
     def xcom_pull_1(**kwargs):
         ti = kwargs('ti')
-        value1 = ti.
+        value1 = ti.xcom_pull(task_ids = 'python_xcom_push_by_return')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값 : ' + value1)
+
+    @task (task_id = 'python_xcom_pull_2') 
+    def xcom_pull_2(status,**kwargs):
+        print('함수 입력값으로 받은 값 : ' + status)
+
+    
+    
+    python_xcom_push_by_return = xcom_push_result()
+    xcom_pull_2(python_xcom_push_by_return)
+    python_xcom_push_by_return >> xcom_pull_1()

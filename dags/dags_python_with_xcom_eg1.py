a098affb7e8a395f246d83767058604dc3c74b60
RimSM <rsm9085@gmail.com>
2026-04-29 22:10:00 +0900

    print문 추가

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -27,5 +27,7 @@ with DAG (
         ti = kwargs['ti']
         value1 = ti.xcom_pull(key = 'result1', task_ids = 'python_xcom_push_task2')
         value2 = ti.xcom_pull(key = 'result2', task_ids = 'python_xcom_push_task1')
+        print(value1)
+        print(value2)
 
     xcom_push1() >> xcom_push2() >> xcom_pull()

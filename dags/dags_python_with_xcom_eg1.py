f7c95b6567f23da1d0c24c494293896a23705cd2
RimSM <rsm9085@gmail.com>
2026-04-29 22:06:21 +0900

    오타 수정

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.decorators import task
 
 with DAG (
-    task_id = "dags_python_with_xcom_eg1", 
+    dag_id = "dags_python_with_xcom_eg1", 
     schedule="30 6 * * *", 
     start_date=pendulum.datetime(2026, 4, 28, tz = 'Asia/Seoul'),
     catchup = False
--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 
 
 with DAG(
-    task_id = 'dags_python_with_xcom_eg2',
+    dag_id = 'dags_python_with_xcom_eg2',
     schedule="10 6 * * *",
     start_date=pendulum.datetime(2026,4,29, tz = 'Asia/Seoul'), 
     catchup = False

15fef0daeeb33551c8321d18bd019825bbc619db
RimSM <rsm9085@gmail.com>
2026-05-02 21:48:18 +0900

    pull push 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -18,9 +18,9 @@ with DAG(
     bash_pull = BashOperator(
         task_id = 'bash_pull', 
         env={
-            'STATUS': '{{ti.xcom_pull(task_ids = "python_pull")["status"]}}',
-            'DATA' : '{{ti.xcom_pull(task_ids = "python_pull")["data"]}}',
-            'OPTION_CNT' : '{{ti.xcom_pull(task_ids = "python_pull")["option_cnt"]}}'
+            'STATUS': '{{ti.xcom_pull(task_ids = "python_push")["status"]}}',
+            'DATA' : '{{ti.xcom_pull(task_ids = "python_push")["data"]}}',
+            'OPTION_CNT' : '{{ti.xcom_pull(task_ids = "python_push")["option_cnt"]}}'
         },
         bash_command='echo $STATUS && echo $DATA && echo $OPTION_CNT'
     )

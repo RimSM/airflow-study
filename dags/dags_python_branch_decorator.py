f6a63f6875bef440f98d038bc77b0916758a2666
RimSM <rsm9085@gmail.com>
2026-05-05 22:01:37 +0900

    branch decorator

---
+++ dags/dags_python_branch_decorator.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id = 'dags_python_branch_decorator',
+    start_date = pendulum.datetime(2026, 5, 5, tz = 'Asia/Seoul'),
+    schedule=None, 
+    catchup=False
+) as dag :
+    
+    @task.branch(task_id ='dags_python_branch_decorator')
+    def select_random(): 
+        import random 
+
+        item_list = ['A','B','C']
+        select_item = random.choice(item_list)
+
+        if select_item == 'A':
+            return 'task_A'
+        elif select_item in ['B','C']:
+            return ['task_B', 'task_C']
+        
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+    task_a = PythonOperator(
+        task_id = 'task_a', 
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )    
+    
+    task_b = PythonOperator(
+        task_id = 'task_b', 
+        python_callable = common_func, 
+        op_kwargs={'selected' : 'B'}
+    )
+
+    task_c = PythonOperator(
+        task_id = 'task_c', 
+        python_callable = common_func, 
+        op_kwargs={'selected' : 'C'}
+    )
+
+    select_random() >> [task_a, task_b, task_c]

eff022c909566b9e81d254033bc8330ec0442601
RimSM <rsm9085@gmail.com>
2026-05-10 16:35:17 +0900

    오타 수정"
    ㄹㅇ
    wq
    qq
    q
    
    dfasd
    
    "수정

--- dags/dags_seoul_api_tbm.py
+++ dags/dags_seoul_api_tbm.py
@@ -12,7 +12,7 @@ with DAG(
     tax_info = SeoulApiToCsvOperator(
         task_id = 'tax_info', 
         dataset_nm = 'FiosTbmRevdesc',
-        path = '/opt/airflow/files/tbm/{{data_interval_end.in_timezone("Asia/Seoul") | ds_no_dash}}', 
+        path = '/opt/airflow/files/tbm/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}', 
         file_name = 'tax_info.csv'
     )
 

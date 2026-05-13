b81f467e5c07e4e0b1b49ec4d796495c92eed3d3
RimSM <rsm9085@gmail.com>
2026-05-13 21:40:42 +0900

    오타 수정

--- dags/dags_python_with_postgres_hook.py
+++ dags/dags_python_with_postgres_hook.py
@@ -4,7 +4,7 @@ from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
-    dag_id ='dags_python_with_postgres', 
+    dag_id ='dags_python_with_postgres_hook', 
     start_date = pendulum.datetime(2026, 5, 13, tz = 'Asia/Seoul'),
     schedule = None, 
     catchup = False
